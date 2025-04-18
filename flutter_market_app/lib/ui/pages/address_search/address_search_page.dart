4b4f4c6ad2a32e42666939fc49d3359406d22c33
lepapillon90 <135020709+lepapillon90@users.noreply.github.com>
2025-04-18 23:40:57 +0900

    2

---
+++ flutter_market_app/lib/ui/pages/address_search/address_search_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AddressSearchPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(), 
+      body: Text('Address Search Page'),
+    );
+  }
+}
---
+++ flutter_market_app/lib/ui/pages/produck_write/produck_write_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ProduckWritePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(), 
+      body: Text('ProduckWritePage'),
+    );
+  }
+}
