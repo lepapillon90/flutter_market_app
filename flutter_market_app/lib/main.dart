f4176809d600c36a235d4f17dfc9ce10d5454a5e
lepapillon90 <135020709+lepapillon90@users.noreply.github.com>
2025-04-22 23:47:26 +0900

    4

--- flutter_market_app/lib/main.dart
+++ flutter_market_app/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_market_app/ui/pages/home/home_page.dart';
+import 'package:flutter_market_app/ui/pages/join/join_page.dart';
 import 'package:flutter_market_app/ui/pages/welcome/welcome_page.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -64,6 +66,7 @@ class MyApp extends StatelessWidget {
           }),
         ),
       ),
+      //home: HomePage(),
       home: WelcomePage(),
     );
   }
